00463dae3ee3a181a07b20e4e25e20f2b89dc128
kirill <kirill.zubov96@yandex.ru>
2023-03-04 16:34:07 +0300

    add tests

---
+++ src/test/java/ru/otus/dao/author/AuthorDaoJbdcTest.java
@@ -0,0 +1,36 @@
+package ru.otus.dao.author;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
+import ru.otus.domain.author.Author;
+import ru.otus.domain.genre.Genre;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JdbcTest
+@Import({AuthorDaoJdbc.class})
+public class AuthorDaoJbdcTest {
+
+    private static final int EXPECTED_AUTHORS_COUNT = 1;
+    private static final String EXPECTED_AUTHOR_NAME = "Автор";
+    @Autowired
+    private AuthorDao authorDao;
+
+    @DisplayName("возвращать ожидаемое количество авторов в БД")
+    @Test
+    void shouldReturnExpectedAuthorsCount() {
+        int actualGenresCount = authorDao.count();
+        assertThat(actualGenresCount).isEqualTo(EXPECTED_AUTHORS_COUNT);
+    }
+
+    @DisplayName("возвращать ожидаемого автора по его id")
+    @Test
+    void shouldReturnExpectedAuthorsById() {
+        Author expectedAuthor = new Author(EXPECTED_AUTHORS_COUNT, EXPECTED_AUTHOR_NAME);
+        Author actualAuthor = authorDao.getById(expectedAuthor.getId());
+        assertThat(actualAuthor).usingRecursiveComparison().isEqualTo(expectedAuthor);
+    }
+}
---
+++ src/test/resources/data.sql
@@ -0,0 +1,5 @@
+insert into authors (`name`) values ('Автор');
+
+insert into genres (`name`) values ('Жанр');
+
+insert into books (`name`, author_id, genre_id) values ('Книга', 1, 1);
